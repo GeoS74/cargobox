20d7fc760b7c193bde475566ba6310d5076a9de8
GeoS74 <gsirotkin@yandex.ru>
2024-01-07 21:48:58 +0300

    dev

---
+++ src/app.ts
@@ -0,0 +1,6 @@
+import Koa from 'koa';
+import config from './config';
+
+const app = new Koa();
+
+export default app;
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import {logger} from './libs/logger'
+import config from './config'
+import app from './app'
+
+app.listen(config.server.port, () => {
+  logger.info(`server run http://${config.server.host}:${config.server.port}`);
+})
+.on('error', error => {
+  logger.error(error.message);
+});
+
+ 
