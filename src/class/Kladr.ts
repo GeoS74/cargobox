abb663e8b9e6be711bc1c7612e55a3670af6899b
geos <gsirotkin@yandex.ru>
2024-01-18 11:01:08 +0500

    added extract 7z archive KLADR

--- src/class/Kladr.ts
+++ src/class/Kladr.ts
@@ -1,5 +1,7 @@
 import https from 'https';
 import { writeFile, unlink } from 'fs/promises';
+import sevenBin from '7zip-bin';
+import { extractFull } from 'node-7z';
 import config from '../config';
 import Bot from './Bot';
 import { loggerChildProcess } from '../libs/logger';
@@ -27,28 +29,39 @@ export default class Kladr extends Bot {
     this.state = 'run';
     this.error = undefined;
 
-    // try {
-    //   await this.downloadKLADR();
-    // } catch (error) {
-    //   if (error instanceof Error) {
-    //     loggerChildProcess.error(`update KLADR: ${error.message}`);
-    //     this.error = error;
-    //   }
-    // }
-    //
-    // this.state = 'wait';
+    try {
+      await this.downloadKLADR().catch((error) => { throw error; });
+      await this.extractKLADR().catch((error) => { throw error; });
+    } catch (error) {
+      if (error instanceof Error) {
+        loggerChildProcess.error(`update KLADR: ${error.message}`);
+        this.error = error;
+      }
+    }
+
+    this.state = 'wait';
+  }
+
+  async extractKLADR() {
+    return new Promise((res, rej) => {
+      const seven = extractFull('./temp/kladrdb.7z', './temp/extract/kladr/', {
+        $bin: sevenBin.path7za, // путь к скрипту распаковки архива
+        $defer: false, // не создавать дочерний процесс
+      });
+      seven.once('end', () => res(1));
+      seven.once('error', (error) => rej(error));
+    });
   }
 
   async downloadKLADR() {
     return new Promise((res, rej) => {
       https.get(`${config.catalog.kladr.db}`, async (response) => {
         if (response.statusCode !== 200) {
-          return rej(new Error(`response status ${response.statusCode}`));
+          rej(new Error(`response status ${response.statusCode}`));
         }
-        await writeFile('./temp/kladrdb.7z', response);
-        return res(1);
+        await writeFile('./temp/kladrdb.7z', response).catch((error) => rej(error));
+        res(1);
       });
-    })
-      .catch((error) => { throw error; });
+    });
   }
 }
