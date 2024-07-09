3dcf14f89f6151b7dc1d6e30c211aa8f3f03487f
geos <gsirotkin@yandex.ru>
2024-07-09 19:40:01 +0500

    added package @koa/cors

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import Koa from 'koa';
+import cors from "@koa/cors"
+import config from "./config"
 import errorCatcher from './middleware/error.catcher';
 import kladrRoutes from './routes/kladr.routes';
 import dellineRoutes from './routes/delline.routes';
@@ -6,6 +8,9 @@ import dellineRoutes from './routes/delline.routes';
 const app = new Koa();
 
 app.use(errorCatcher);
+if (config.node.env === 'dev') {
+  app.use(cors());
+}
 app.use(kladrRoutes);
 app.use(dellineRoutes);
 
