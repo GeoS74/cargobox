6262a0e1b120318ea322cede0e57ea743c959b87
geos <gsirotkin@yandex.ru>
2024-07-11 18:35:16 +0500

    added trim method in city validator

--- src/middleware/validatros/city.validator.ts
+++ src/middleware/validatros/city.validator.ts
@@ -4,6 +4,6 @@ export async function checkCity(ctx: Context, next: Next) {
   if (typeof ctx.query.city !== 'string' || !ctx.query.city.length) {
     ctx.throw(404, 'city not found');
   }
-  ctx.city = ctx.query.city;
+  ctx.city = ctx.query.city.trim();
   await next();
 }
